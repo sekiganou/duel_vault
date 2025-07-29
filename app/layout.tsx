1a8bb7e30b9971cb71fe9083d094a83aacdca5cf
Alessio Bagno <alessiobagno@gmail.com>
2025-07-29 21:19:41 +0000

    Change theme and add sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { Providers } from "./providers";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
 
 export const metadata: Metadata = {
   title: {
@@ -43,7 +44,7 @@ export default function RootLayout({
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
-            <Navbar />
+            <Sidebar />
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
               {children}
             </main>
--- lib/api/tests.ts
+++ lib/api/tests.ts
@@ -1,17 +1,18 @@
 import { type table_name } from "@/generated/prisma";
 import axios from "axios";
+import { useState } from "react";
 
 export async function getAllTests(): Promise<table_name[]> {
   const res = await axios.get("/api/tests");
   return res.data;
 }
 
-export async function createTest(name: string): Promise<void> {
+export async function createTest(name: string) {
   await axios.post("/api/tests", {
     name,
   });
 }
 
-export async function deleteTest(id: number): Promise<void> {
-  await axios.delete(`/api/tests?id=${id}`);
+export async function deleteTest(id: number) {
+  await axios.delete(`/api/tests?id=${id}`).then();
 }
