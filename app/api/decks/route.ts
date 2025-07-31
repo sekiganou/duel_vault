55e92b2ed44d439d9afd67626dae1fb31472a244
Alessio Bagno <alessiobagno@gmail.com>
2025-07-31 10:45:36 +0000

    Init minio client when needed and cache it

--- app/api/decks/route.ts
+++ app/api/decks/route.ts
@@ -1,7 +1,7 @@
 import { db } from "@/db";
 import { withErrorHandler } from "@/lib/middlewares/withErrorHandler";
 import { CreateDeckSchema } from "@/lib/schemas/decks";
-import { minioClient, S3_BUCKET } from "@/s3";
+import { getMinioClient, S3_BUCKET } from "@/s3";
 import { NextRequest, NextResponse } from "next/server";
 import z from "zod";
 
@@ -14,6 +14,8 @@ export const GET = withErrorHandler(async () => {
       format: true,
     },
   });
+
+  const minioClient = getMinioClient();
 
   await Promise.all(
     items.map(async (item) => {
--- s3/index.ts
+++ s3/index.ts
@@ -2,10 +2,21 @@ import * as Minio from "minio";
 
 export const S3_BUCKET = "duel-vault";
 
-export const minioClient = new Minio.Client({
-  endPoint: process.env.MINIO_HOST!,
-  port: 9000,
-  useSSL: process.env.NODE_ENV !== "development",
-  accessKey: process.env.MINIO_ROOT_USER!,
-  secretKey: process.env.MINIO_ROOT_PASSWORD!,
-});
+let cachedMinioclient: Minio.Client | null = null;
+
+export const getMinioClient = () => {
+  if (!process.env.MINIO_HOST) {
+    throw new Error("Missing MINIO_HOST environment variable");
+  }
+
+  return (
+    cachedMinioclient ||
+    new Minio.Client({
+      endPoint: process.env.MINIO_HOST,
+      port: 9000,
+      useSSL: process.env.NODE_ENV !== "development",
+      accessKey: process.env.MINIO_ROOT_USER,
+      secretKey: process.env.MINIO_ROOT_PASSWORD,
+    })
+  );
+};
